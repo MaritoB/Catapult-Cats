4149b989e094747e7d55a0074c96d28b1f92aef2
Francisco Acosta Grafenstein <103229571+FranciscoGrafenstein@users.noreply.github.com>
2023-04-22 04:18:06 -0300

    New Canvas and camera improvement

--- Catapult Cats/Assets/Scripts/CameraController.cs
+++ Catapult Cats/Assets/Scripts/CameraController.cs
@@ -6,12 +6,12 @@ public class CameraController : MonoBehaviour
 {
   [SerializeField] private Transform playerTransform; // Objeto que seguirá la cámara
     [SerializeField] private Transform cannonTransform; // Objeto que representa el cañón
-    [SerializeField] private float smoothSpeed = 0.125f; // Velocidad de la cámara
+    [SerializeField] private float smoothSpeed = 2.5f; // Velocidad de la cámara
     [SerializeField] private float offset = 3f; // Distancia de la cámara respecto al objeto que sigue
-    [SerializeField] private float minCameraX = -100f; // Límite mínimo de la cámara en el eje X
-    [SerializeField] private float maxCameraX = 100f; // Límite máximo de la cámara en el eje X
-    [SerializeField] private float minCameraY = -10f; // Altura mínima de la cámara
-    [SerializeField] private float maxCameraY = 100f; // Altura máxima de la cámara
+    [SerializeField] private float minCameraX = -10f; // Límite mínimo de la cámara en el eje X
+    [SerializeField] private float maxCameraX = 10f; // Límite máximo de la cámara en el eje X
+    [SerializeField] private float minCameraY = -1f; // Altura mínima de la cámara
+    [SerializeField] private float maxCameraY = 0.2f; // Altura máxima de la cámara
     [SerializeField] private float fastSpeed = 10f; // Velocidad de movimiento rápido
     [SerializeField] private float minCameraZ = -100f; // Límite mínimo de la cámara en el eje Z
     [SerializeField] private float maxCameraZ = 100f; // Límite máximo de la cámara en el eje Z

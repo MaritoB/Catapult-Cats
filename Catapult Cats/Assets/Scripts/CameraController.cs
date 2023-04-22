2131fc84bd71b324c4aa7b0132021021b5fcdcc0
Francisco Acosta Grafenstein <103229571+FranciscoGrafenstein@users.noreply.github.com>
2023-04-22 04:12:48 -0300

    Camera Movement Implementation
    New object Camera and CAmeraController script

---
+++ Catapult Cats/Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+  [SerializeField] private Transform playerTransform; // Objeto que seguirá la cámara
+    [SerializeField] private Transform cannonTransform; // Objeto que representa el cañón
+    [SerializeField] private float smoothSpeed = 0.125f; // Velocidad de la cámara
+    [SerializeField] private float offset = 3f; // Distancia de la cámara respecto al objeto que sigue
+    [SerializeField] private float minCameraX = -100f; // Límite mínimo de la cámara en el eje X
+    [SerializeField] private float maxCameraX = 100f; // Límite máximo de la cámara en el eje X
+    [SerializeField] private float minCameraY = -10f; // Altura mínima de la cámara
+    [SerializeField] private float maxCameraY = 100f; // Altura máxima de la cámara
+    [SerializeField] private float fastSpeed = 10f; // Velocidad de movimiento rápido
+    [SerializeField] private float minCameraZ = -100f; // Límite mínimo de la cámara en el eje Z
+    [SerializeField] private float maxCameraZ = 100f; // Límite máximo de la cámara en el eje Z
+
+
+    private bool isMovingFast; // Flag que indica si la cámara se está moviendo rápidamente
+
+    private void LateUpdate()
+    {
+    Vector3 desiredPosition = new Vector3(
+            Mathf.Clamp(playerTransform.position.x, minCameraX, maxCameraX),
+            Mathf.Clamp(playerTransform.position.y + offset, minCameraY, maxCameraY),
+            Mathf.Clamp(transform.position.z, minCameraZ, maxCameraZ)
+        );
+
+        float currentSpeed = isMovingFast ? fastSpeed : smoothSpeed;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, currentSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+
+        if (Input.GetMouseButton(0))
+        {
+            isMovingFast = true;
+        }
+        else if (isMovingFast)
+        {
+            isMovingFast = false;
+            Vector3 cannonPosition = cannonTransform.position;
+            cannonPosition.z = transform.position.z;
+            transform.position = Vector3.Lerp(transform.position, cannonPosition, fastSpeed * Time.deltaTime);
+        }
+    }
+}
